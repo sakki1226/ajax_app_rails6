2a23ba3d4ebe97ed0ee48b228f712cd6d25366b7
sakki1226 <142205272+sakki1226@users.noreply.github.com>
2024-01-31 16:22:51 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,17 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  return html;
+};
+
 function post() {
   const form = document.getElementById("form");
   form.addEventListener("submit", (e) => {
@@ -22,7 +36,17 @@ function post() {
     });
 
     XHR.send(formData);
+    XHR.onload = () => {
+      if (XHR.status != 200){
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");
+      list.insertAdjacentElement("afterend", build(XHR));
+      formText.value = "";
+    };
   });
-}
+};
 
 window.addEventListener('turbo:load', post);
